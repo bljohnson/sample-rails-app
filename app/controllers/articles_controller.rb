ae2b16282090ffd143f160491ee063a0086a5d98
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-09-03 08:52:29 -0500

    Add New Article framework

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,5 @@
+# a controller is a class defined to inherit from ApplicationController. Inside class you define methods that become actions for this controller. Actions perform CRUD operations on articles within system.
+class ArticlesController < ApplicationController
+	def new
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # tells Rails to map requests to the root of the application to the welcome controller's index action
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :articles
 end
