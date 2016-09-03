4acdb4cf33d23122e56cf38c4474cb7189ec0652
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-09-02 21:09:19 -0500

    Create controller, action, view

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+	# tells Rails to map requests to http://localhost:3000/welcome/index to the welcome controller's index action
+  get 'welcome/index'
+  # tells Rails to map requests to the root of the application to the welcome controller's index action
+  root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
