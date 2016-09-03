6891bb026daf539cb472ee22b3a3918382f50188
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-09-03 10:44:34 -0500

    Add edit ability

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,6 +15,10 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	# use Article model to save data in db
 	def create
 		@article = Article.new(article_params)
@@ -23,6 +27,16 @@ class ArticlesController < ApplicationController
 			redirect_to @article
 		else
 			render 'new'
+		end
+	end
+
+	def update
+		@article = Article.find(params[:id])
+
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render 'edit'
 		end
 	end
 
