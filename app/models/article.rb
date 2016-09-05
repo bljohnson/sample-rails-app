50bb1a4455ea5f75949571b641539176e049945e
Bethany Johnson <johnson.l.bethany@gmail.com>
2016-09-04 19:28:24 -0500

    Generate and associate comments model

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 # Article class inherits from ApplicationRecord. ApplicationRecord inherits from ActiveRecord::Base which supplies a great deal of functionality to your Rails models for free, including basic database CRUD (Create, Read, Update, Destroy) operations, data validation, as well as sophisticated search support and the ability to relate multiple models to one another
 class Article < ApplicationRecord
+	has_many :comments
 	# ensure that all articles have a title that is at least five characters long
 	validates :title, presence: true,
 				  length: { minimum: 5 }
